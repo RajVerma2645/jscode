25f5b169044cb8a6859e3cc2eb2025d4f1456e0f
RajVerma2645 <112325929+RajVerma2645@users.noreply.github.com>
2022-10-11 19:50:29 +0530

    Add files via upload

---
+++ Assign10102022.js
@@ -0,0 +1,19 @@
+/*Last date for fee submission is over and now late fine will be incurred, so you are assigned the
+ job to build a late fine calculator for a coaching institution:
+• fine 5% if the fee value is between 50 and 300
+• fine 10% if the fee value is higher
+*/
+
+const lateFineCalculator = function (feeValue) {
+    if (feeValue < 50 )  // Fee should not be less than 50 as it is the min fees
+        return "Invalid value Input"
+    let lateFine = (feeValue >= 50 && feeValue < 100)? fine=5 : fine =10;  //if fees is between 50 & 100, then 
+                                                                             // fine % = 5, else fine % 10
+    return lateFine = ((feeValue*fine)/100)+feeValue 
+}
+
+const lateFine = lateFineCalculator(3000);
+if (typeof lateFine != "string")            //if return value is not string then print latefine and value
+console.log("Total fee payable incl Fine = $", lateFine)
+else 
+console.log(lateFine)                       // else Print Invalid input
